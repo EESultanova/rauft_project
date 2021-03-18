e7015852efbef012f9674533aa2cc76e436c31c8
Artem Chebotarev <artp956@gmail.com>
2021-03-18 14:51:06 +0300

    navbar+footer

--- routes/pages.js
+++ routes/pages.js
@@ -1,1 +1,11 @@
-//Здесь будут ручки на все страницы Артем
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/training', (req, res) => {
+  res.render('training');
+});
+
+module.exports = router;
