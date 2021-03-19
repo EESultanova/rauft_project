cf15c1e3314a28fa930177259fc62528151626ae
Elya <esultanova96@gmail.com>
2021-03-19 13:32:24 +0300

    sss

--- middlewares/checkAdmin.js
+++ middlewares/checkAdmin.js
@@ -1,30 +1,27 @@
-// Делает Эля, но на всякий случай сделаем, так как был незабываемый творческий порыв
+ const User = require('../db/models/User')
 
+ const bcrypt = require('bcrypt')
 
-//  const User = require('../db/models/User')
+const saltRound = 10
 
-//  const bcrypt = require('bcrypt')
+ const checkAdmin = async (req, res, next) => {
+   const { name, email, password, age, education, login } = req.body;
+    if(email === 'admin@admin.com') {
+      const pass = await bcrypt.hash(password, saltRound);
+      const admin = new User ({ name, email, password: pass, age, education, login, role: 0 });
+      await admin.save();
+      return res.redirect ('/')
+    } else {
+      return next()
+    }
+ }
 
-// const saltRound = 10
-
-//  const checkAdmin = async (req, res, next) => {
-//    const { name, email, password, age, education, login } = req.body;
-//     if(email === 'admin@admin.com') {
-//       const pass = await bcrypt.hash(password, saltRound);
-//       const admin = new User ({ name, email, password: pass, age, education, login, role: 0 });
-//       await admin.save();
-//       return res.redirect ('/')
-//     } else {
-//       return next()
-//     }
-//  }
-
-// module.exports = checkAdmin
+//module.exports = checkAdmin
 
 
 
  
-const checkAdmin = async (req, res, next) => {
+const checkIsAdmin = async (req, res, next) => {
   const userId = req.session?.user?.id
 
   if (userId) {
@@ -39,6 +36,7 @@ const checkAdmin = async (req, res, next) => {
   return res.redirect('/')
 }
 
-module.exports = {
-  checkAdmin,
-}
+module.exports = 
+  checkIsAdmin,
+  checkAdmin
+
